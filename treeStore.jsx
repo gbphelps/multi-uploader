a31d5c41903136b21d1283808718c491bda2a9e7
Grant Phelps <gbphelps2@gmail.com>
2020-02-10 09:17:35 -0500

    Fix error - forgot to update visibleRows of the actual child in treeStore

--- treeStore.jsx
+++ treeStore.jsx
@@ -34,34 +34,33 @@ function createStore(){
 
     function toggle(idxs){
         let entry = state[idxs[0]];
-        const parents = [entry];
+        const parents = [];
 
         for (let i=1; i<idxs.length; i++){
-            entry = entry.children[idxs[i]];
             parents.unshift(entry);
+            entry = entry.children[idxs[i]];
         }
-
-        //TODO: for some reason this bugs out unless I set twice? ook at the visibleRows console log, that seems to be part of the problem rather than React maybe?
 
         const oldSize = getSize(entry);
         entry.expanded = !entry.expanded;
         const newSize = getSize(entry);
+        entry.visibleRows = newSize;
+        //TODO clean this up
+
+        console.log(parents.map(p => p.visibleRows));
+
+        for (let i=0; i<parents.length; i++){
+            parents[i].visibleRows += newSize - oldSize;
+            const indexes = [idxs[0]].concat(parents[i].idxs);
+            subscriptions[JSON.stringify(indexes)].setState({
+                visibleRows: parents[i].visibleRows,
+            })
+        }
 
         subscriptions[JSON.stringify(idxs)].setState({
             open: entry.expanded,
             visibleRows: newSize,
         })
-
-        for (let i=0; i<parents.length; i++){
-            parents[i].visibleRows += newSize - oldSize;
-            console.log(parents[i].visibleRows);
-            
-            const indexes = [idxs[0]].concat(parents[i].idxs);
-
-            subscriptions[JSON.stringify(indexes)].setState({
-                visibleRows: parents[i].visibleRows,
-            })
-        }
     }
 
     function subscribe(self, idxs){
